064669e5df4426d1337b1c83f6d4232a8d1bcf0f
Michelle Noorali <michellemolu@gmail.com>
2019-10-28 20:18:58 -0400

    add ref to go service, handle async calls, cleanup

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const request = require('request');
 const exphbs = require('express-handlebars')
+const axios = require('axios');
 
 const port = process.argv.slice(2)[0];
 const app = express();
+
+//const tipService = process.env.TIP_SERVICE;
+//const tipService = process.env.GO_SERVICE;
+const tipService = 'http://localhost:8082';
+const goService = 'http://localhost:8080';
 
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
@@ -17,14 +22,26 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(bodyParser.json());
 
-const heroesService = 'http://localhost:8081';
+function getTip() {
+  return axios.get(`${tipService}/tips`);
+}
 
+function getGoGreeting() {
+  return axios.get(`${goService}`);
+}
 
 app.get('/', (req, res) => {
   console.log('hitting index path');
-  res.render('home', {
-    name: 'Michelle'
-  })
+
+  axios.all([getTip(), getGoGreeting()])
+  .then(axios.spread(function(tipResp, greetingResp) {
+    res.render('home', {
+      version: process.env.EXAMPLE_NODE_APP_VERSION,
+      tipServiceVersion: process.env.TIP_SERVICE_VERSION,
+      tip: tipResp.data,
+      goGreeting: greetingResp.data,
+      })
+  }));
 });
 
 app.use('/img', express.static(path.join(__dirname,'img')));
